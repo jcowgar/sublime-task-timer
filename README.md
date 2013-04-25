1d4f94bfd27c05e565813ef6687c018121f164d7
Jeremy Cowgar <jeremy@cowgar.com>
2013-04-25 05:11:04 -0400

    Added timer pause and stop commands

--- README.md
+++ README.md
@@ -1,12 +1,11 @@
 # Sublime Task Timer
 
-A plugin created for Sublime to time simple tasks such as how long
-it takes to fix a given bug. When work begins on a task, run
-the command `Task Timer: Start Timer`. Any time you wish to determine
-how long the timer has been running for, run the command
-`Task Timer: Status`. If you wish to restart the timer at any given
-moment, simply run the command `Task Timer: Start Timer` to restart
-the timer.
+A plug-in created for Sublime Text to time simple tasks such as how long
+it takes to fix a given bug.
+
+This plug-in gives you the ability to start, pause, stop and check the status
+of a timer. When you pause, stop or check the status of a timer the elapsed
+time is displayed as hours, minutes and seconds in the status bar.
 
 ## For Help
 
@@ -22,8 +21,20 @@ The initial release.
 
 Copyright (c) 2013 Jeremy Cowgar
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- TaskTimer.py
+++ TaskTimer.py
@@ -1,36 +1,83 @@
 from datetime import datetime
-import sublime, sublime_plugin
+
+import sublime
+from sublime_plugin import ApplicationCommand
 
 
-class TaskTimerStartCommand(sublime_plugin.ApplicationCommand):
+def increment_seconds():
+    if sublime.task_timer_time is not None:
+        td = datetime.now() - sublime.task_timer_time
+        sublime.task_timer_seconds += (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10 ** 6) / 10 ** 6
+
+
+def report_time():
+    has_run = hasattr(sublime, 'task_timer_seconds')
+    if has_run is False or (has_run and sublime.task_timer_seconds == 0):
+        sublime.status_message('task timer not running')
+        return
+
+    seconds = sublime.task_timer_seconds
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    output = []
+    if hours > 1:
+        output.append('%i hours' % hours)
+    elif hours == 1:
+        output.append('1 hour')
+
+    if minutes > 1:
+        output.append('%i minutes' % minutes)
+    elif minutes == 1:
+        output.append('1 minute')
+
+    if seconds > 1:
+        output.append('%i seconds' % seconds)
+    elif seconds == 1:
+        output.append('1 second')
+
+    sublime.status_message(' '.join(output))
+
+
+class TaskTimerStartCommand(ApplicationCommand):
     def run(self):
+        if not hasattr(sublime, 'task_timer_seconds'):
+            sublime.task_timer_seconds = 0
         sublime.task_timer_time = datetime.now()
 
 
-class TaskTimerStatusCommand(sublime_plugin.ApplicationCommand):
+class TaskTimerPauseCommand(ApplicationCommand):
+    """A sublime text command to pause the timer"""
     def run(self):
         try:
-            td = datetime.now() - sublime.task_timer_time
-            seconds = (td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6
+            increment_seconds()
 
-            hours = seconds // 3600
-            minutes = (seconds % 3600) // 60
-            seconds = seconds % 60
+            report_time()
 
-            output = []
-            if hours > 1:
-                output.append('%i hours' % hours)
-            elif hours == 1:
-                output.append('1 hour')
-            if minutes > 1:
-                output.append('%i minutes' % minutes)
-            elif minutes == 1:
-                output.append('1 minute')
-            if seconds > 1:
-                output.append('%i seconds' % seconds)
-            elif seconds == 1:
-                output.append('1 second')
+            sublime.task_timer_time = None
 
-            sublime.status_message(' '.join(output))
         except:
-            sublime.status_message('task timer not running')
+            sublime.status_message("task timer not running")
+
+
+class TaskTimerStopCommand(ApplicationCommand):
+    """A sublime text command to stop the timer"""
+    def run(self):
+        increment_seconds()
+
+        report_time()
+
+        sublime.task_timer_seconds = 0
+        sublime.task_timer_time = None
+
+
+class TaskTimerStatusCommand(ApplicationCommand):
+    def run(self):
+        increment_seconds()
+
+        report_time()
+
+        if sublime.task_timer_time is not None:
+            sublime.task_timer_time = datetime.now()
